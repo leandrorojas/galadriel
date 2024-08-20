f929c38c9607c45c3c91fa2cf1bee0a07880da88
Leandro Rojas <leandrorojas@gmail.com>
2024-08-20 11:53:17 -0300

    stating with base page

--- galadriel/galadriel.py
+++ galadriel/galadriel.py
@@ -6,12 +6,100 @@ class State(rx.State):
 
     ...
 
-def index() -> rx.Component:
-    # Welcome Page (Index)
+def navbar_link(text: str, url: str) -> rx.Component:
+    return rx.link(
+        rx.text(text, size="4", weight="medium"), href=url
+    )
+
+def navbar() -> rx.Component:
+    return rx.box(
+        rx.desktop_only(
+            rx.hstack(
+                rx.hstack(
+                    rx.image(
+                        src="/logo.jpg",
+                        width="2.25em",
+                        height="auto",
+                        border_radius="25%",
+                    ),
+                    rx.heading(
+                        "Reflex", size="7", weight="bold"
+                    ),
+                    align_items="center",
+                ),
+                rx.hstack(
+                    navbar_link("Home", "/#"),
+                    navbar_link("About", "/#"),
+                    navbar_link("Pricing", "/#"),
+                    navbar_link("Contact", "/#"),
+                    spacing="5",
+                ),
+                rx.hstack(
+                    rx.button(
+                        "Sign Up",
+                        size="3",
+                        variant="outline",
+                    ),
+                    rx.button("Log In", size="3"),
+                    spacing="4",
+                    justify="end",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+        rx.mobile_and_tablet(
+            rx.hstack(
+                rx.hstack(
+                    rx.image(
+                        src="/logo.jpg",
+                        width="2em",
+                        height="auto",
+                        border_radius="25%",
+                    ),
+                    rx.heading(
+                        "Reflex", size="6", weight="bold"
+                    ),
+                    align_items="center",
+                ),
+                rx.menu.root(
+                    rx.menu.trigger(
+                        rx.icon("menu", size=30)
+                    ),
+                    rx.menu.content(
+                        rx.menu.item("Home"),
+                        rx.menu.item("About"),
+                        rx.menu.item("Pricing"),
+                        rx.menu.item("Contact"),
+                        rx.menu.separator(),
+                        rx.menu.item("Log in"),
+                        rx.menu.item("Sign up"),
+                    ),
+                    justify="end",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+        bg=rx.color("accent", 3),
+        padding="1em",
+        # position="fixed",
+        # top="0px",
+        # z_index="5",
+        width="100%",
+    )
+
+def base_page(content: rx.Component, *args) -> rx.Component:
     return rx.container(
+        navbar(),
         rx.color_mode.button(position="top-right"),
+        content,
+        rx.logo(),
+    )
+
+def index() -> rx.Component:
+    return base_page(
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
@@ -26,7 +114,6 @@ def index() -> rx.Component:
             justify="center",
             min_height="85vh",
         ),
-        rx.logo(),
     )
 
 app = rx.App(
