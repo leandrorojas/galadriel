f721e87186958411181257ce6a94dd2c096979f2
Leandro Rojas <leandrorojas@gmail.com>
2024-09-04 20:20:09 -0300

    finished test suite list page

--- galadriel/suite/list.py
+++ galadriel/suite/list.py
@@ -4,8 +4,9 @@ import reflex as rx
 from .. import navigation
 from . import state, model
 from ..pages import base_page
+from ..ui import components
 
-def suite_detail_link(child: rx.Component, suite: model.SuiteModel):
+def __suite_detail_link(child: rx.Component, suite: model.SuiteModel):
 
     if suite is None:
         return rx.fragment(child)
@@ -22,25 +23,77 @@ def suite_detail_link(child: rx.Component, suite: model.SuiteModel):
         href=suite_detail_url
     )
 
-def suite_list_item(suite: model.SuiteModel):
+def __suite_list_item(suite: model.SuiteModel):
     return rx.box(
-        suite_detail_link(
+        __suite_detail_link(
             rx.heading(suite.name),
             suite
         ),
         padding="1em"
     )
 
+def __show_suite(suite:model.SuiteModel):
+    return rx.table.row(
+         rx.table.cell(__suite_detail_link(suite.name, suite)),
+         rx.table.cell(suite.created),
+    )
+
+def __add_suite_button() -> rx.Component:
+    return rx.fragment(
+        rx.link(
+            rx.button(
+                rx.icon("plus", size=26), 
+                rx.text("Add Suite", size="4", display=["none", "none", "block"]), 
+                size="3", 
+            ),
+            href=navigation.routes.SUITE_ADD
+        ), 
+    )
+
+def __header_cell(text: str, icon: str):
+    return rx.table.column_header_cell(
+        rx.hstack(
+            rx.icon(icon, size=18),
+            rx.text(text),
+            align="center",
+            spacing="2",
+        ),
+    )    
+
+def __table() -> rx.Component:
+    return rx.fragment(
+        rx.table.root(
+            rx.table.header(
+                rx.table.row(
+                    __header_cell("name", "beaker"),
+                    __header_cell("created", "calendar-check-2"),
+                ),
+            ),
+            rx.table.body(rx.foreach(state.SuiteState.suites, __show_suite)),
+            variant="surface",
+            size="3",
+            width="100%",
+            on_mount=state.SuiteState.load_suites,
+        ),
+    )
+
 def suites_list_page() -> rx.Component:
+    title_badge = components.Badge()
 
     return base_page(
         rx.vstack(
-            rx.heading("Test Suites"),
-            rx.link(
-                rx.button("New Suite"),
-                href=navigation.routes.SUITE_ADD
-            ),            
-            rx.foreach(state.SuiteState.suites, suite_list_item),
+            rx.flex(
+                title_badge.title("beaker", "Test Suites"),
+                rx.spacer(),
+                rx.hstack(__add_suite_button(),),
+                spacing="2",
+                flex_direction=["column", "column", "row"],
+                align="center",
+                width="100%",
+                top="0px",
+                padding_top="2em",       
+            ),
+            __table(),
             spacing="5",
             align="center",
             min_height="85vh"
