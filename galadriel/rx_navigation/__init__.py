82935cfe2da3d223357f048263c041d3514ba509
Leandro Rojas <leandrorojas@gmail.com>
2024-09-06 12:32:14 -0300

    duplicated tutorial objects

---
+++ galadriel/rx_navigation/__init__.py
@@ -0,0 +1,13 @@
+from . import routes
+from .state import NavigationState
+
+from . import rx_routes
+from .rx_state import RxTutorialNavigationState
+
+__all__ = [
+    'rx_routes',
+    'routes',
+    'rx_routes',
+    'RxTutorialNavigationState',
+    'NavigationState'
+]
