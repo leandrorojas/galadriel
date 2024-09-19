43ee9e625595a6a362c6d6526e807936a4687809
Leandro Rojas <leandrorojas@gmail.com>
2024-09-19 09:49:50 -0300

    added step model

---
+++ galadriel/step/__init__.py
@@ -0,0 +1,5 @@
+from .model import StepModel
+
+__all__ = [
+    'StepModel',
+]
---
+++ galadriel/step/model.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import sqlalchemy as sa
+from sqlmodel import Field
+import reflex as rx
+
+from .. import utils
+
+class StepModel(rx.Model, table=True):
+    case_id:int = Field(foreign_key="casemodel.id")
+    order:int
+    action:str
+    expected:str
+    created: datetime = Field(
+        default_factory=utils.timing.get_utc_now, 
+        sa_type=sa.DateTime(timezone=True),
+        sa_column_kwargs={
+            'server_default': sa.func.now()
+        },
+        nullable=False
+    )
+    deleted: datetime = Field(
+        sa_type=sa.DateTime(timezone=True),
+        nullable=True
+    )
