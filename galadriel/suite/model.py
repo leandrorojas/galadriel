36a4c39f49df033a8cfa2d03ead9b17b11cb571d
Leandro Rojas <leandrorojas@gmail.com>
2024-10-04 17:55:14 -0300

    moved suite child type model to suite model file

--- galadriel/suite/model.py
+++ galadriel/suite/model.py
@@ -20,3 +20,14 @@ class SuiteModel(rx.Model, table=True):
         nullable=True
     )
     #created_by
+
+class SuiteChildTypeModel(rx.Model, table=True):
+    type_name:str
+    created: datetime = Field(
+        default_factory=utils.timing.get_utc_now, 
+        sa_type=sa.DateTime(timezone=True),
+        sa_column_kwargs={
+            'server_default': sa.func.now()
+        },
+        nullable=False
+    )
