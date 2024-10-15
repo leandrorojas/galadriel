e66f95b6da7872f6aa55becd73d8fda78c3cf870
Leandro Rojas <leandrorojas@gmail.com>
2024-10-15 14:44:22 -0300

    added iteration status model

---
+++ galadriel/iteration/model.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import sqlalchemy as sa
+from sqlmodel import Field
+import reflex as rx
+
+from .. import utils
+
+class IterationStatusModel(rx.Model, table=True): 
+    name: str
+    created: datetime = Field(
+        default_factory=utils.timing.get_utc_now, 
+        sa_type=sa.DateTime(timezone=True),
+        sa_column_kwargs={
+            'server_default': sa.func.now()
+        },
+        nullable=False
+    )
+
+    def dict(self, *args, **kwargs) -> dict:
+        """Serialize method."""
+        d = super().dict(*args, **kwargs)
+        d["created"] = self.created.replace(microsecond=0).isoformat(sep=" ")
+        return d
