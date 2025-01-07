d1f7782da82118a1f00082ff1ea9675d7b8d2bf2
Leandro Rojas <leandrorojas@gmail.com>
2025-01-07 11:12:35 -0300

    "added edit generic page"

---
+++ galadriel/pages/edit.py
@@ -0,0 +1,63 @@
+import reflex as rx
+
+from ..ui.components import Badge
+from ..pages import base_page
+
+def __case_list_button(to_list_button:str, link:str) -> rx.Component:
+    return rx.fragment(
+        rx.link(
+            rx.button(
+                rx.icon("chevrons-left", size=26), 
+                rx.text(to_list_button, size="4", display=["none", "none", "block"]), 
+                size="3", 
+            ),
+            href=link
+        ),
+    )
+
+def __case_detail_button(to_detail_button:str, link:str) -> rx.Component:
+    return rx.fragment(
+        rx.link(
+            rx.button(
+                rx.icon("chevron-left", size=26), 
+                rx.text(to_detail_button, size="4", display=["none", "none", "block"]), 
+                size="3", 
+            ),
+            href=link
+        )
+    )
+
+def edit_page(form:rx.Component, title:str, title_icon:str, to_list_button:str, to_detail_button:str, list_link:str, detail_link:str) -> rx.Component:
+    my_form = form()
+    title_badge = Badge()
+
+    case_edit_content = rx.vstack(
+        rx.flex(
+            title_badge.title(title_icon, title),
+            rx.spacer(),
+            rx.hstack(__case_list_button(to_list_button, list_link),__case_detail_button(to_detail_button, detail_link)),            
+            spacing="2",
+            flex_direction=["column", "column", "row"],
+            align="center",
+            width="100%",
+            top="0px",
+            padding_top="2em",       
+        ),
+        rx.desktop_only(
+            rx.box( 
+                my_form,
+                width="50vw",
+            ),
+        ),
+        rx.mobile_and_tablet(
+            rx.box(
+                my_form,
+                width="55vw"
+            ),
+        ),
+        spacing="5",
+        align="center",
+        min_height="95vh",
+    ),
+    
+    return base_page(case_edit_content)
--- galadriel/suite/edit.py
+++ galadriel/suite/edit.py
@@ -1,68 +1,11 @@
 import reflex as rx
 import reflex_local_auth
 
-from . import forms
+from .forms import suite_edit_form
 from .state import EditSuiteState
-from ..pages import base_page
 from ..navigation import routes
-from ..ui.components import Badge
-
-def __suite_list_button() -> rx.Component:
-    return rx.fragment(
-        rx.link(
-            rx.button(
-                rx.icon("chevrons-left", size=26), 
-                rx.text("to Suites", size="4", display=["none", "none", "block"]), 
-                size="3", 
-            ),
-            href=routes.SUITES
-        ), 
-    )
-
-def __suite_detail_button() -> rx.Component:
-    return rx.fragment(
-        rx.link(
-            rx.button(
-                rx.icon("chevron-left", size=26), 
-                rx.text("back to Detail", size="4", display=["none", "none", "block"]), 
-                size="3", 
-            ),
-            href=EditSuiteState.suite_url
-        ), 
-    )
+from ..pages.edit import edit_page
 
 @reflex_local_auth.require_login
 def suite_edit_page() -> rx.Component:
-    my_form = forms.suite_edit_form()
-    title_badge = Badge()
-
-    suite_edit_content = rx.vstack(
-        rx.flex(
-            title_badge.title("beaker", "Edit Test Suite"),
-            rx.spacer(),
-            rx.hstack(__suite_list_button(),__suite_detail_button()),
-            spacing="2",
-            flex_direction=["column", "column", "row"],
-            align="center",
-            width="100%",
-            top="0px",
-            padding_top="2em",
-        ),
-        rx.desktop_only(
-            rx.box( 
-                my_form,
-                width="50vw",
-            ),
-        ),
-        rx.mobile_and_tablet(
-            rx.box(
-                my_form,
-                width="55vw"
-            ),
-        ),
-        spacing="5",
-        align="center",
-        min_height="95vh",
-    ),
-    
-    return base_page(suite_edit_content)
+    return edit_page(suite_edit_form, "Edit Test Suite", "route", "to Suites", "to Suite Detail", routes.SUITES, EditSuiteState.suite_url)
