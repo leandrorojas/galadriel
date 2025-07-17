bd975c725be1f88a040377f8bf22c94e7a17658f
Leandro Rojas <leandrorojas@gmail.com>
2025-07-17 10:11:55 -0300

    "fixed CycleModel to use timing standard"

--- galadriel/cycle/model.py
+++ galadriel/cycle/model.py
@@ -3,7 +3,8 @@ import sqlalchemy as sa
 from sqlmodel import Field
 import reflex as rx
 
-from .. import utils
+#from .. import utils
+from ..utils import timing, consts
 
 class CycleModel(rx.Model, table=True):
     name: str
@@ -11,7 +12,7 @@ class CycleModel(rx.Model, table=True):
     iteration_status_name:str = Field(nullable=True)
     cycle_status_name: str = Field(nullable=True)
     created: datetime = Field(
-        default_factory=utils.timing.get_utc_now, 
+        default_factory=timing.get_utc_now, 
         sa_type=sa.DateTime(timezone=True),
         sa_column_kwargs={
             'server_default': sa.func.now()
@@ -26,13 +27,13 @@ class CycleModel(rx.Model, table=True):
     def dict(self, *args, **kwargs) -> dict:
         """Serialize method."""
         d = super().dict(*args, **kwargs)
-        d["created"] = self.created.replace(microsecond=0).isoformat(sep=" ")
+        d["created"] = timing.ensure_utc(self.created).replace(microsecond=0).isoformat(sep=" ")
         return d
 
 class CycleChildTypeModel(rx.Model, table=True):
     type_name:str
     created: datetime = Field(
-        default_factory=utils.timing.get_utc_now, 
+        default_factory=timing.get_utc_now, 
         sa_type=sa.DateTime(timezone=True),
         sa_column_kwargs={
             'server_default': sa.func.now()
@@ -43,13 +44,13 @@ class CycleChildTypeModel(rx.Model, table=True):
     def dict(self, *args, **kwargs) -> dict:
         """Serialize method."""
         d = super().dict(*args, **kwargs)
-        d["created"] = self.created.replace(microsecond=0).isoformat(sep=" ")
+        d["created"] = timing.ensure_utc(self.created).replace(microsecond=0).isoformat(sep=" ")
         return d
     
 class CycleStatusModel(rx.Model, table=True):
     status_name: str
     created: datetime = Field(
-        default_factory=utils.timing.get_utc_now, 
+        default_factory=timing.get_utc_now, 
         sa_type=sa.DateTime(timezone=True),
         sa_column_kwargs={
             'server_default': sa.func.now()
@@ -60,7 +61,7 @@ class CycleStatusModel(rx.Model, table=True):
     def dict(self, *args, **kwargs) -> dict:
         """Serialize method."""
         d = super().dict(*args, **kwargs)
-        d["created"] = self.created.replace(microsecond=0).isoformat(sep=" ")
+        d["created"] = timing.ensure_utc(self.created).replace(microsecond=0).isoformat(sep=" ")
         return d
 
 class CycleChildModel(rx.Model, table=True):
@@ -70,7 +71,7 @@ class CycleChildModel(rx.Model, table=True):
     order:int
     child_name:str = Field(nullable=True)
     created: datetime = Field(
-        default_factory=utils.timing.get_utc_now, 
+        default_factory=timing.get_utc_now, 
         sa_type=sa.DateTime(timezone=True),
         sa_column_kwargs={
             'server_default': sa.func.now()
@@ -81,5 +82,5 @@ class CycleChildModel(rx.Model, table=True):
     def dict(self, *args, **kwargs) -> dict:
         """Serialize method."""
         d = super().dict(*args, **kwargs)
-        d["created"] = self.created.replace(microsecond=0).isoformat(sep=" ")
+        d["created"] = timing.ensure_utc(self.created).replace(microsecond=0).isoformat(sep=" ")
         return d
