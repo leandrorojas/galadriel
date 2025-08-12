0c45333f88544e674643bcdf5c2081ae0372affd
Leandro Rojas <leandrorojas@gmail.com>
2025-08-12 08:08:41 -0300

    "added control prints"

--- galadriel/galadriel.py
+++ galadriel/galadriel.py
@@ -51,13 +51,14 @@ app = rx.App(
     },
 )
 
-print("before first run check")
+print("BEFORE first run check")
 if (yaml.read_setting("galadriel.yaml", "galadriel", "first_run") == None):
+    print("INSIDE first run check")
     seed = install.seed
     seed.seed_db()
     seed.set_first_run()
     yaml.write_setting("galadriel.yaml", "galadriel", "first_run", 1)
-print("after first run check")
+print("AFTER first run check")
 
 app.add_page(index, title="galadriel")
 
